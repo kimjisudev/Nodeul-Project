6a8a3eaebbce2ce56a6d5af1fd0e45cd2044083c
hsnkch <chsick@naver.com>
2023-05-31 12:27:49 +0900

    qwe

--- NodeulProject/src/main/java/com/bookitaka/NodeulProject/member/dto/UserDataDTO.java
+++ NodeulProject/src/main/java/com/bookitaka/NodeulProject/member/dto/UserDataDTO.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@PasswordMatch(passwordField = "memberPassword", passwordConfirmField = "memberPwChk")
+@PasswordMatch(passwordField = "memberPassword", passwordConfirmField = "memberPasswordCheck")
 public class UserDataDTO {
 
   @ApiModelProperty(position = 0)
@@ -29,7 +29,7 @@ public class UserDataDTO {
   @NotBlank(message = "비밀번호를 입력해주세요.")
   @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*\\W)(?=\\S+$).{6,12}",
           message = "비밀번호는 영문자와 숫자, 특수기호가 적어도 1개 이상 포함된 6자~12자의 비밀번호여야 합니다.")
-  private String memberPwChk;
+  private String memberPasswordCheck;
   @ApiModelProperty(position = 3)
   @NotBlank(message = "이름을 입력해주세요.")
   private String memberName;
