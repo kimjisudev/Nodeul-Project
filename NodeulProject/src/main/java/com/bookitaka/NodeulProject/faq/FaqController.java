46b407c1f052d6928e117441b50b1f3cb1ee10d5
hsnkch <chsick@naver.com>
2023-06-15 17:00:50 +0900

    faq : list 관리자일때 버튼 표시

--- NodeulProject/src/main/java/com/bookitaka/NodeulProject/faq/FaqController.java
+++ NodeulProject/src/main/java/com/bookitaka/NodeulProject/faq/FaqController.java
@@ -1,5 +1,10 @@
 package com.bookitaka.NodeulProject.faq;
 
+import com.bookitaka.NodeulProject.member.controller.MemberAPIController;
+import com.bookitaka.NodeulProject.member.model.Member;
+import com.bookitaka.NodeulProject.member.model.MemberRoles;
+import com.bookitaka.NodeulProject.member.security.Token;
+import com.bookitaka.NodeulProject.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -11,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.UnsupportedEncodingException;
@@ -24,6 +30,7 @@ public class FaqController {
 
     private final FaqService service;
     private final ModelMapper modelMapper;
+    private final MemberService memberService;
 
     // 1대1 문의 정적 페이지
     @GetMapping("/qna")
@@ -36,9 +43,9 @@ public class FaqController {
     // FAQ 카테고리 별 목록
     @GetMapping("/{faqCategory}")
     public String list(@PathVariable String faqCategory,
-                       Model model,
                        @RequestParam(name = "page", defaultValue = "0") int page,
-                       @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+                       @RequestParam(name = "keyword", defaultValue = "") String keyword,
+                       HttpServletRequest request, Model model) {
         model.addAttribute("faqCategory", faqCategory);
         model.addAttribute("faqAllCategory", service.getAllFaqCategory());
         model.addAttribute("currentPage", page);
@@ -80,6 +87,14 @@ public class FaqController {
             nextGroupStartPage = endPage;
         }
         model.addAttribute("nextGroupStartPage", nextGroupStartPage);
+
+        // 관리자 확인
+        Member whoami = memberService.whoami(request.getCookies(), Token.ACCESS_TOKEN);
+        if (whoami != null && whoami.getMemberRole().equals(MemberRoles.ADMIN)) {
+            model.addAttribute("role",  whoami.getMemberRole());
+            log.info("================================================ memberRole : {}", whoami.getMemberRole());
+        }
+
         return "/faq/faqList";
     }
 
