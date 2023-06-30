2f2dee487bf8d94ba6e6c767d86dd82701b0cfb3
BeomSeok Yu <beomseok.dev@gmail.com>
2023-07-01 00:17:52 +0900

    업로드 폴더 생성 문제 수정

--- src/main/java/com/bookitaka/NodeulProject/InitUploadDirMaker.java
+++ src/main/java/com/bookitaka/NodeulProject/InitUploadDirMaker.java
@@ -1,5 +1,6 @@
 package com.bookitaka.NodeulProject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class InitUploadDirMaker {
     @Value("${file.bookImg.dir}")
@@ -26,11 +28,14 @@ public class InitUploadDirMaker {
         folders.add(new File(sheetFileDir));
         folders.add(new File(previewDir));
 
+        log.info("========= Make UpFolder ==========");
         for (File folder : folders) {
             // 해당 디렉토리가 없다면 디렉토리를 생성.
             if (!folder.exists()) {
                 try {
-                    folder.mkdirs(); //폴더 생성합니다. ("새폴더"만 생성)
+                    log.info("folder : {}", folder.getAbsolutePath());
+                    boolean result = folder.mkdirs(); //폴더 생성합니다. ("새폴더"만 생성)
+                    log.info("result : {}", result);
                 } catch (Exception e) {
                     e.getStackTrace();
                 }
